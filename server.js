600f498ff10548cd7c0a4b09541d6ced570d8e95
Alec Willits <alecwillits@gmail.com>
2024-07-16 20:04:56 -0600

    updated cors

--- server.js
+++ server.js
@@ -12,36 +12,52 @@ const port = process.env.PORT || 3000;
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
 
+// CORS Configuration
 app.use(cors({
-  origin: 'https://tradevault-frontend.onrender.com',
+  origin: 'http://localhost:4200',
   optionsSuccessStatus: 200
 }));
 
 // Check if MONGO_URI is loaded
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI).then(() => {
-  console.log('Connected to MongoDB');
-}).catch(err => {
-  console.error('Error connecting to MongoDB:', err);
-});
+if (!mongoURI) {
+  console.error('MONGO_URI not defined in environment variables');
+  process.exit(1);
+}
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(err => {
+    console.error('Error connecting to MongoDB:', err);
+    process.exit(1);
+  });
 
 // Routes
 app.use('/sales', saleRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+// Serve static files from the Angular app
+app.use(express.static(path.join(__dirname, 'dist/trade-vault-frontend')));
+
+// Angular app wildcard route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/trade-vault-frontend/index.html'));
 });
 
+// Root route
 app.get('/', (req, res) => {
   res.send('Welcome to TradeVault Backend');
 });
 
-// Serve static files from the Angular app
-app.use(express.static(path.join(__dirname, 'dist/trade-vault-frontend')));
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/trade-vault-frontend/index.html'));
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
