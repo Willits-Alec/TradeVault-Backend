342ff8bbd6484a7ad72c31690cc5a831d99c14c2
Alec Willits <alecwillits@gmail.com>
2024-07-16 20:23:38 -0600

    new wildcard route path

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.static(path.join(__dirname, 'dist/trade-vault-frontend')));
 
 // Angular app wildcard route
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/trade-vault-frontend/index.html'));
+  res.sendFile(path.join(__dirname, 'dist/trade-vault-frontend/browser/index.html'));
 });
 
 // Root route
